3f625ca2741a311f3b319fea9f6cdf27c8984ec0
Moeed Rafi <moeedrafi95@gmail.com>
2026-04-20 21:36:16 +0500

    ui: request  page

---
+++ frontend/src/app/(app)/requests/RequestSearch.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export const RequestSearch = () => {
+  return (
+    <div>
+      <input
+        onChange={() => {}}
+        placeholder="Search for users"
+        className="w-full text-sm bg-light px-3 py-2 rounded-xl ring-1 ring-color focus-visible:ring-2 outline-none disabled:cursor-not-allowed disabled:opacity-50"
+      />
+    </div>
+  );
+};
--- frontend/src/app/(app)/requests/page.tsx
+++ frontend/src/app/(app)/requests/page.tsx
@@ -1,7 +1,15 @@
+import { RequestList } from "./RequestList";
+import { RequestSearch } from "./RequestSearch";
+
 export default function RequestsPage() {
   return (
-    <div className="flex-1 h-full flex items-center justify-center font-lato text-2xl text-text">
-      Requests Page
+    <div className="flex-1 h-full px-4 py-6 font-lato text-2xl text-text">
+      <div className="max-w-5xl mx-auto space-y-6">
+        <h1 className="text-3xl font-semibold tracking-tight">Requests</h1>
+
+        <RequestSearch />
+        <RequestList />
+      </div>
     </div>
   );
 }
